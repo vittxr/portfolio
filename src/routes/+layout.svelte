a1efb6d22970ce2f8175e3bf17c274102ccfec20
vittxr <vitor.roberto3022@gmail.com>
2024-03-17 19:09:56 -0300

    change adapter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
   import { browser } from '$app/environment'
   import '$lib/i18n' 
   import { locale, waitLocale } from 'svelte-i18n'
+  import { dev } from '$app/environment';
+
+  // vercel analytics config
+  import { inject } from '@vercel/analytics';
+  inject({ mode: dev ? 'development' : 'production' });
 
   export const load = async () => {
     if (browser) {
