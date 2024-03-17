4f94278635bfa487bf049c5e7888067abfa30e18
vittxr <vitor.roberto3022@gmail.com>
2024-03-17 18:03:16 -0300

    i18n is working

---
+++ src/routes/common/Header/LangDropdown.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { _ } from 'svelte-i18n'
+    import { locale } from 'svelte-i18n'
+
+    const changeLang = (e) => {
+        locale.set(e.target.value)
+    }
+</script>
+
+<div>
+    <select id="lang" name="lang" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md" on:change={changeLang}>
+      <option value="en">{$_('lang_options.en')}</option>
+      <option value="pt">{$_('lang_options.pt')}</option>
+    </select>
+</div>
