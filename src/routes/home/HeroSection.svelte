90981171fb2d494f1985ebd3229a125602e4778f
vittxr <vitor.roberto3022@gmail.com>
2024-03-25 21:02:25 -0300

    working in task: hero section

--- src/routes/home/HeroSection.svelte
+++ src/routes/home/HeroSection.svelte
@@ -5,34 +5,34 @@
   import { _ } from 'svelte-i18n';
 </script>
 
-<div class="sm:flex mt-10">
-  <div class="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4 rounded-full">
-    <img src="a" alt={$_('images_alt.profile')} />
+<div class="sm:flex mt-10 items-center">
+  <div class="w-32 h-full mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
+    <img src="imgs/pfp.jpg" alt={$_('images_alt.profile')} class="w-full h-full rounded-full" />
   </div>
   <div>
     <h4 class="text-lg font-bold">Vitor Schirmer</h4>
     <p class="mt-1">
       {$_('role')}
     </p>
-  </div>
-</div>
 
-<div class="mt-2">
-  <p>
-    {$_('hero_section.description')}
-  </p>
+    <div class="mt-2">
+      <p>
+        {$_('hero_section.description')}
+      </p>
 
-  <div class="py-4 flex space-x-2">
-    <a href="https://github.com/vittxr" target="_blank">
-      <GithubIcon className="w-6 h-6" />
-    </a>
+      <div class="py-4 flex space-x-2">
+        <a href="https://github.com/vittxr" target="_blank">
+          <GithubIcon className="w-6 h-6" />
+        </a>
 
-    <a href="https://www.linkedin.com/in/vitor-roberto-8b68a9223/" target="_blank">
-      <LinkedinIcon className="w-6 h-6" />
-    </a>
+        <a href="https://www.linkedin.com/in/vitor-roberto-8b68a9223/" target="_blank">
+          <LinkedinIcon className="w-6 h-6" />
+        </a>
 
-    <a href="https://instagram.com/vitxr.me" target="_blank">
-      <InstagramIcon className="w-6 h-6" />
-    </a>
+        <a href="https://instagram.com/vitxr.me" target="_blank">
+          <InstagramIcon className="w-6 h-6" />
+        </a>
+      </div>
+    </div>
   </div>
 </div>
