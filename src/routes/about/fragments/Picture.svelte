7fcdf5e6e0c6528add32fd40980e268912937648
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-07-08 21:58:42 -0300

    refactor: componentize pfp component

---
+++ src/routes/about/fragments/Picture.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { _ } from 'svelte-i18n';
+</script>
+
+<div class="w-32 h-32 mb-4 flex-shrink-0 sm:mb-0 sm:mr-4 mx-auto">
+  <img
+    src="imgs/pfp2.jpg"
+    alt={$_('images_alt.profile')}
+    class="w-full h-full rounded-full object-cover"
+  />
+</div>
