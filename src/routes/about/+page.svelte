9cfbc4840903fd07bc18c88b24529d96de16e93f
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-07-10 22:14:13 -0300

    feat: rename JobsExperience as Timeline.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,10 @@
 <script>
   import Base from '$routes/_fragments/Base.svelte';
-  import JobsExperience from './fragments/JobsExperience.svelte';
   import { _ } from 'svelte-i18n';
   import Picture from './fragments/Picture.svelte';
   import Link from '$components/Link.svelte';
   import ArrowTopRightOnSquare from '$components/icons/ArrowTopRightOnSquare.svelte';
+  import Timeline from './fragments/Timeline.svelte';
 </script>
 
 <Base>
@@ -26,12 +26,12 @@
       <Picture />
     </div>
 
-    <p class="mt-5">
+    <p class="mt-2">
       I'm a software engineer with a passion for web development. I have experience with
       technologies like React, Vue, Svelte, and Node.js. I'm always looking for new challenges and
       opportunities to learn new things.
     </p>
 
-    <JobsExperience />
+    <Timeline />
   </div>
 </Base>
---
+++ src/routes/about/fragments/Timeline.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Timeline, TimelineItem } from '$components/Timeline';
+  import CalendarIcon from '$components/icons/CalendarIcon.svelte';
+  import SuitcaseIcon from '$components/icons/SuitcaseIcon.svelte';
+  import { _ } from 'svelte-i18n';
+</script>
+
+<div class="mt-2 w-full">
+  s
+  <h3 class="text-2xl font-bold">
+    {$_('about.timeline.title')}
+  </h3>
+  <Timeline initialIcon={CalendarIcon}>
+    {#each $_(`about.timeline.items`) as item, i}
+      <TimelineItem idx={i} icon={SuitcaseIcon}>
+        <div class="flex items-center justify-between text-2xl">
+          <h5>{item.year}</h5>
+          <CalendarIcon className="h-8 w-8 text-gray-500 inline-block mr-1" />
+        </div>
+
+        <div>{item.description}</div>
+      </TimelineItem>
+    {/each}
+  </Timeline>
+</div>
