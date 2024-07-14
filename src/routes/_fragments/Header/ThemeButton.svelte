9cba9e75792fb3fa9ca7afbd2833b3a2184ce1c8
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-07-13 23:40:42 -0300

    fix: fix toogle theme mode button.

--- src/routes/_fragments/Header/ThemeButton.svelte
+++ src/routes/_fragments/Header/ThemeButton.svelte
@@ -1,26 +1,33 @@
-<script>
+<script lang="ts">
   import Switch from '$components/Switch.svelte';
   import { _ } from 'svelte-i18n';
   import SunIcon from '$components/icons/SunIcon.svelte';
   import MoonIcon from '$components/icons/MoonIcon.svelte';
-  import { onMount } from 'svelte';
+  import { browser } from '$app/environment';
 
   let isDarkTheme = false;
   const toggleTheme = () => {
     isDarkTheme = !isDarkTheme;
+    localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
     document.body.classList.toggle('dark', isDarkTheme);
   };
 
-  onMount(() => {
+  if (browser) {
+    const theme = localStorage.getItem('theme');
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    isDarkTheme = prefersDark;
+
+    if (!theme) {
+      isDarkTheme = prefersDark;
+    } else {
+      isDarkTheme = theme === 'dark';
+    }
     document.body.classList.toggle('dark', isDarkTheme);
-  });
+  }
 </script>
 
 <Switch
   onSwitch={toggleTheme}
   srOnly={$_('theme.toggle')}
   icons={{ enabled: SunIcon, disabled: MoonIcon }}
-  defaultValue={isDarkTheme ? 'disabled' : 'enabled'}
+  defaultValue={isDarkTheme ? 'enabled' : 'disabled'}
 />
