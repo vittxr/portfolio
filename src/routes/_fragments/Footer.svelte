d47636fad556554c7b5c33d23ab81fd4a8a8d4d7
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-07-17 21:56:50 -0300

    feat: add footer

---
+++ src/routes/_fragments/Footer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import GithubIcon from '$components/icons/GithubIcon.svelte';
+  import LinkedinIcon from '$components/icons/LinkedinIcon.svelte';
+  import { _ } from 'svelte-i18n';
+</script>
+
+<footer class="flex justify-between">
+  <span class="text-gray-300 text-sm">© 2024 - Vitor Schirmer</span>
+
+  <div class="flex space-x-2">
+    <a href="https://github.com/vittxr" target="_blank">
+      <GithubIcon className="w-5 h-5" />
+    </a>
+
+    <a href="https://www.linkedin.com/in/vschirmer/" target="_blank">
+      <LinkedinIcon className="w-5 h-5" />
+    </a>
+  </div>
+</footer>
