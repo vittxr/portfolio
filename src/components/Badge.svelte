b97717823e4930f59ee24ae1b909d0db6255cc18
Vitor Schirmer <vitor.roberto3022@gmail.com>
2024-08-24 16:39:34 -0300

    feat: add dark mode to <Badge />

--- src/components/Badge.svelte
+++ src/components/Badge.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <span
-  class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-100 text-gray-800 {className}"
+  class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-100 {className}"
 >
   <slot />
 </span>
