e95797b5d1a53a984b0159a6e9be133898578b71
vittxr <vitor.roberto3022@gmail.com>
2026-02-21 19:41:33 -0300

    feat: improve <Tools />

--- src/routes/about/fragments/Tools.svelte
+++ src/routes/about/fragments/Tools.svelte
@@ -3,15 +3,18 @@
   import { marked } from 'marked';
   import { _, isLoading } from 'svelte-i18n';
   import I18nText from '$components/I18nText.svelte';
+  import Link from '$components/Link.svelte';
+  import Title from './fragments/Title.svelte';
+
+  const ghRepo = 'https://raw.githubusercontent.com/vittxr/mytools/main/README.md';
 
   let content = '';
   let loading = true;
   let error = false;
 
   onMount(async () => {
-    console.log('aaaaaaaaaaaaaaaaaaa');
     try {
-      const res = await fetch('https://raw.githubusercontent.com/vittxr/mytools/main/README.md');
+      const res = await fetch(ghRepo);
       if (!res.ok) throw new Error();
       const md = await res.text();
       content = await marked(md);
@@ -27,12 +30,20 @@
   });
 </script>
 
-{#if loading}
-  loading
-{:else if error}
-  <p>Failed to load content.</p>
-{:else}
-  <div class="prose prose-sm max-w-none dark:prose-invert">
-    {@html content}
-  </div>
-{/if}
+<section class="w-full">
+  <Title ancorKey="tools">
+    <I18nText>{$_('about_page.tools.title')}</I18nText>
+  </Title>
+  <h2><I18nText>{$_('about_page.tools.description')}</I18nText></h2>
+
+  {#if loading}
+    loading
+  {:else if error}
+    <p>Failed to load content.</p>
+  {:else}
+    <Link classname="text-xs underline" href={ghRepo}>{ghRepo}</Link>
+    <div class="prose prose-sm max-w-none dark:prose-invert">
+      {@html content}
+    </div>
+  {/if}
+</section>
